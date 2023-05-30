d7493a905d2bd515ce237d926d65ba12a5555196
alejandro <alejandrober96@gmail.com>
2023-05-30 11:35:32 -0500

    Add genres table and FK to music_albums

--- schema.sql
+++ schema.sql
@@ -1,10 +1,17 @@
 	CREATE TABLE music_albums(
-  id SERIAL PRIMARY KEY,
-  publish_date VARCHAR(255),
-  on_spotify BOOLEAN,
-  archived BOOLEAN,
-  genre_id INT,
-  author_id INT,
-  source_id INT,
-  label_id INT
+    id SERIAL PRIMARY KEY,
+    publish_date VARCHAR(40),
+    on_spotify BOOLEAN,
+    archived BOOLEAN,
+    genre_id INT,
+    author_id INT,
+    source_id INT,
+    label_id INT
 );
+
+CREATE TABLE Genres( 
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60)
+);
+
+alter table music_albums add constraint fk_genre_id foreign key (genre_id) references genres(id);
