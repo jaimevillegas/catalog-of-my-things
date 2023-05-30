75c46db125bb55fe0bdf934dfce7f8a360776bff
johnie261 <njorogejohn261@gmail.com>
2023-05-30 14:21:59 +0300

    Fix linters

--- game.rb
+++ game.rb
@@ -2,16 +2,18 @@ require './item'
 require 'date'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(multiplayer, last_played_at, id = nil)
     # Pass last_played_at as publish_date parameter
-    super(last_played_at, id)  
+    super(last_played_at, id)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
     return false if @last_played_at.nil?
+
     super && (Date.today.year - Date.parse(@last_played_at.to_s).year > 2)
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,31 +3,29 @@ require_relative '../author'
 require_relative '../item'
 
 describe Author do
+  context 'Create instance of Author class' do
+    author = Author.new('Johnie', 'Njoroge')
+    item = Item.new('2017-05-09')
 
-    context 'Create instance of Author class' do
-      author = Author.new('Johnie', 'Njoroge')
-      item = Item.new('2017-05-09')
-
-      it 'should return instance of Book class' do
-        expect(author).to be_instance_of(Author)
-      end
-      it 'should throw an ArgumentError if given fewer than 2 parameters' do
-        expect { Author.new('Johnny') }.to raise_error(ArgumentError)
-      end
-      it 'returns the correct first name' do
-        author.first_name.should eql 'Johnie'
-      end
-      it 'returns the correct last name' do
-        author.last_name.should eql 'Njoroge'
-      end
-      it 'Adding items' do
-        author.add_item(item)
-        expect(author.items.size).to eql 1
-      end
-      it 'returns the added item' do
-        author.add_item(item)
-        expect(author.items[0]).to be(item)
-      end
+    it 'should return instance of Book class' do
+      expect(author).to be_instance_of(Author)
     end
-
+    it 'should throw an ArgumentError if given fewer than 2 parameters' do
+      expect { Author.new('Johnny') }.to raise_error(ArgumentError)
+    end
+    it 'returns the correct first name' do
+      author.first_name.should eql 'Johnie'
+    end
+    it 'returns the correct last name' do
+      author.last_name.should eql 'Njoroge'
+    end
+    it 'Adding items' do
+      author.add_item(item)
+      expect(author.items.size).to eql 1
+    end
+    it 'returns the added item' do
+      author.add_item(item)
+      expect(author.items[0]).to be(item)
+    end
+  end
 end
