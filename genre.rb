313b630f5a0228a0d32e2719f499b04b04718313
alejandro <alejandrober96@gmail.com>
2023-05-30 07:50:45 -0500

    linters and corrections on attr_accessor

--- genre.rb
+++ genre.rb
@@ -1,13 +1,15 @@
 class Genre
   attr_accessor :name, :items, :id
 
-  def initialize(id, name, id = nil)
+  def initialize(name, id = nil)
     @id = id || rand(1..1000)
     @name = name
     @items = []
   end
 
   def add_item(item)
+    return unless item.instance_of?(Item) && !@items.include?(item)
+
     @items << item
     item.add_genre = self
   end
--- item.rb
+++ item.rb
@@ -1,8 +1,8 @@
 require 'date'
 
 class Item
-  attr_accessor :publish_date
-  attr_reader :id, :genre, :author, :label, :archived
+  attr_accessor :publish_date, :genre, :label
+  attr_reader :id, :author, :archived
 
   def initialize(publish_date, id = nil)
     @id = id || rand(1..1000)
