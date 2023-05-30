44395ea3c3a538861d7eb7d96413580469abfae8
alejandro <alejandrober96@gmail.com>
2023-05-30 10:36:40 -0500

    modification of add_music_album_method

--- modules/music_and_genres.rb
+++ modules/music_and_genres.rb
@@ -19,7 +19,13 @@ module MusicAndGenres
   end
 
   # create a method for add a music album
-  def add_music_album(id, publish_date, on_spotify)
+  def add_music_album
+    puts 'Please enter the genre id'
+    genre_id = gets.chomp.to_i
+    puts 'Please enter the pushlish date'
+    publish_date = gets.chomp
+    puts 'Please enter if the album is on spotify'
+    on_spotify = gets.chomp
     music_album = MusicAlbum.new(id, publish_date, on_spotify)
     @music_albums << music_album
   end
