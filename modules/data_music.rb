fc7c856f05ff095c1e994ab22f255e1e48a65fcc
alejandro <alejandrober96@gmail.com>
2023-05-30 10:52:35 -0500

    Correction the route

--- modules/data_music.rb
+++ modules/data_music.rb
@@ -15,25 +15,25 @@ module DataMusic
   end
 
   def store_music_albums
-    File.write('albums.json', @music_albums.map { |album| album_to_hash(album) }.to_json)
+    File.write('data/albums.json', @music_albums.map { |album| album_to_hash(album) }.to_json)
   end
 
   def store_genres
-    File.write('genres.json', @genres.map { |genre| genre_to_hash(genre) }.to_json)
+    File.write('data/genres.json', @genres.map { |genre| genre_to_hash(genre) }.to_json)
   end
 
   def load_music_albums
-    return unless File.exist?('albums.json') && File.size?('albums.json')
+    return unless File.exist?('data/albums.json') && File.size?('albums.json')
 
-    JSON.parse(File.read('albums.json')).each do |album|
+    JSON.parse(File.read('data/albums.json')).each do |album|
       @music_albums << MusicAlbum.new(album['id'], album['publish_date'], album['on_spotify'])
     end
   end
 
   def load_genres
-    return unless File.exist?('genres.json') && File.size?('genres.json')
+    return unless File.exist?('data/genres.json') && File.size?('genres.json')
 
-    JSON.parse(File.read('genres.json')).each do |genre|
+    JSON.parse(File.read('data/genres.json')).each do |genre|
       @genres << Genre.new(genre['id'], genre['name'])
     end
   end
